bb5f2c6be0e358ada5aac9683a9658a8f2aa93ac
TommasoBambagiotti <tommaso.bambagiotti@gmail.com>
2026-03-10 17:11:07 +0100

    Added implementation for sparse tri-diagonal linear system

--- OdeSolverBV.py
+++ OdeSolverBV.py
@@ -1,5 +1,7 @@
 import numpy as np
-from scipy.linalg import eigh_tridiagonal
+from scipy.linalg import eigh_tridiagonal, solve
+from scipy.sparse import diags
+from scipy.sparse.linalg import spsolve
 
 
 class OdeSolverBV:
@@ -90,23 +92,93 @@ class FiniteDifference(OdeSolverBV):
         # first compute p(x_i) for i=1,...,N then remove p(x_1)
         inf_diag = -1 - (self.dx/2)*self.f(x_int)[0][1:]
 
-        # IMPLEMENT WITH SPARSE MATRIX!!!
         A = np.diag(princ_diag, k=0) + \
             np.diag(sup_diag, k=1) + \
-            np.diag(inf_diag, k=-1)   
+            np.diag(inf_diag, k=-1)
+        
+        # Solve
+        self.u[1:-1] = solve(A,b)
 
-        # Solve the matrix using numpy
+        return self.x, self.u, A, b
+    
+    def solve_sparse(self, x_span, N):
+        """Solve the second order linera ODE using finite difference method
+        for sparse linear system A x = b
 
-        self.u[1:-1] = np.linalg.solve(A,b)
+        Args:
+            x_span (tuple): left and right values x_a <= x <= x_b
+            N (integer): number of interior mesh points
+
+        Returns:
+            x (array): x axis
+            u (array): approximate solution
+        """
+
+        # compute h=(xb-xa)/(N+1) with N+2 points
+        x_a, x_b = x_span
+        self.dx = (x_b-x_a)/(N+1)
+
+        # create coordinate array and approximate solution array
+        self.x = np.linspace(x_a, x_b, N+2)
+        self.u = np.zeros(N+2)
+
+        # set boundary values
+        # .x[0] = x_a
+        # self.x[N+1] = x_b
+        self.u[0] = self.u_a
+        self.u[N+1] = self.u_b
+
+        # need only interior mesh points
+        x_int = self.x[1:-1]  # remove first and last elements
+
+        # define coefficients vector
+        # we use f=(p,q,r) tuple of the coefficient functions
+        # f(x)[0]=p, f(x)[1]=q, f(x)[2]=r array
+        b = np.zeros(N)  # same dimension of x_int
+        b[0] = -(self.dx**2)*self.f(x_int[0])[2] + \
+                (1+(self.dx/2)*self.f(x_int[0])[0])*self.u_a
+
+        b[N-1] = -(self.dx**2)*self.f(x_int[N-1])[2] + \
+                  (1-(self.dx/2)*self.f(x_int[N-1])[0])*self.u_b
+
+        b[1:-1] = -(self.dx**2)*self.f(x_int[1:-1])[2]
+
+        # tridiagonal matrix -> sparse matrix...?
+        # first compute p(x_i) for i=1,...,N then remove p(x_N)
+        sup_diag = -1 + (self.dx/2)*self.f(x_int)[0][:-1]
+        # compute q(x_i) for i=1,...,N
+        princ_diag = 2 + (self.dx**2)*self.f(x_int)[1]
+        # first compute p(x_i) for i=1,...,N then remove p(x_1)
+        inf_diag = -1 - (self.dx/2)*self.f(x_int)[0][1:]
+
+        # Write A as sparse matrix
+        A = diags([inf_diag, princ_diag, sup_diag], offsets=[-1, 0, 1], format='csc')
+    
+        # Solve for sparse matrix A
+        self.u[1:-1] = spsolve(A,b)
 
         return self.x, self.u, A, b
 
 
-class FiniteDifferenceHomo(OdeSolverBV):
+class FiniteDifferenceHomo(OdeSolverBV):  
     def __init__(self, V):
+        """_summary_
+
+        Args:
+            V (_type_): _description_
+        """        
         self.V = V
 
     def solve(self, x_span, N):
+        """_summary_
+
+        Args:
+            x_span (_type_): _description_
+            N (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """        
         # Same as in the general finite difference method
         # compute h=(xb-xa)/(N+1) with N+2 points
         x_a, x_b = x_span
@@ -133,12 +205,14 @@ class FiniteDifferenceHomo(OdeSolverBV):
         # optimised for symmetric tridiagonal matrices
         w, H = eigh_tridiagonal(princ_diag, off_diag)
 
-        # add last point and normalise
-        # WIP
-
         return self.dx, self.x, w, H
 
 class Numerov(OdeSolverBV):
+    """_summary_
+
+    Args:
+        OdeSolverBV (_type_): _description_
+    """    
 
     def __init__(self):
         raise NotImplementedError("Numerov's algorithm for linear boundary problems" \
